23af1baa241ac5b9fc0370fead6b61407077edbc
fxie <feilian1000@126.com>
2021-03-16 12:13:26 +0800

    Version 1.0 (c)

--- TestDataPreprocessing.py
+++ TestDataPreprocessing.py
@@ -8,10 +8,10 @@ df.loc[df['类别'] == 0, 'IsTypeOne'] = 1
 df['IsTypeTwo'] = 0
 df.loc[df['类别'] == 1, 'IsTypeTwo'] = 1
 
-df_1 = df[['2', 'IsTypeOne']][0:20000]
+df_1 = df[['2', 'IsTypeOne']][0:100000]
 df_1 = df_1.rename(columns={"IsTypeOne": "IsType"})
-df_1.to_csv('./Datasets/Dataset_1.csv', index=False)
+df_1.to_csv('./Datasets/Test_1.csv', index=False)
 
-df_2 = df[['2', 'IsTypeTwo']][0:20000]
+df_2 = df[['2', 'IsTypeTwo']][0:100000]
 df_2 = df_2.rename(columns={"IsTypeTwo": "IsType"})
-df_2.to_csv('./Datasets/Dataset_2.csv', index=False)
+df_2.to_csv('./Datasets/Test_2.csv', index=False)
